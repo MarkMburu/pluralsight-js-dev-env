0a9caf17e20d5a0ef131d1bb42eace4da126dbc4
MarkMburu <markmburu685@gmail.com>
2019-07-02 09:44:21 +0300

    Completed Module 14

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,7 @@
 export default function getBaseUrl(){
     // const inDevelopment = window.location.hostname === 'localhost';
     // return inDevelopment ? 'http://localhost:3001/' : '/';
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/': '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/': 'https://tragically-goose-97072.herokuapp.com/';
 }
 function getQueryStringParameterByName(name, url) {
     if (!url) url = window.location.href;
